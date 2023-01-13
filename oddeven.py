8849d232fab0c456f3f7185b806bc7f5aedb8e14
Joseph Morales <117571756+JosephMorales28@users.noreply.github.com>
2023-01-13 08:08:12 +0800

    Add files via upload

---
+++ oddeven.py
@@ -0,0 +1,35 @@
+x = int(input())
+print("with less than equal sign")
+while x <= 10:
+    if x%2 == 0:
+        print(x,"is even")
+    else:
+        print(x,"is odd")
+
+    x += 1 
+"""print 
+1 is odd
+2 is even
+3 is odd
+4 is even
+5 is odd
+6 is even
+7 is odd
+8 is even
+9 is odd
+10 is even
+"""
+"""
+explanation 
+1 0+1=1
+2 1+1=2
+3 2+1=3
+4 3+1=4
+5 4+1=5
+6 5+1=6
+7 6+1=7
+8 7+1=8
+9 8+1=9
+10 9+1=10
+because number 10 joined
+"""
